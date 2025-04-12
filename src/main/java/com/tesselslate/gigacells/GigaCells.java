e6e1fb207aed5003e403508d4da4045cbe306f02
tesselslate <46545045+tesselslate@users.noreply.github.com>
2025-04-12 15:52:51 -0400

    feat: implement storage cells
    No textures or language files yet

--- src/main/java/com/tesselslate/gigacells/GigaCells.java
+++ src/main/java/com/tesselslate/gigacells/GigaCells.java
@@ -1,33 +1,24 @@
 package com.tesselslate.gigacells;
 
-import com.mojang.logging.LogUtils;
-
-import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.world.item.Item;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
-import net.minecraftforge.registries.DeferredRegister;
-import net.minecraftforge.registries.ForgeRegistries;
+
 import org.slf4j.Logger;
+
+import appeng.api.storage.StorageCells;
+import com.mojang.logging.LogUtils;
 
 @Mod(GigaCells.MODID)
 public class GigaCells {
     public static final String MODID = "gigacells";
-    private static final Logger LOGGER = LogUtils.getLogger();
-
-    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MODID);
+    static final Logger LOGGER = LogUtils.getLogger();
 
     public GigaCells() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
-        ITEMS.register(modEventBus);
+        GigaCellsItems.init(modEventBus);
 
-        modEventBus.addListener(GigaCells::initStorageCells);
-    }
-
-    private static void initStorageCells(FMLCommonSetupEvent event) {
-        // TODO
+        StorageCells.addCellHandler(LargeCellHandler.INSTANCE);
     }
 }
